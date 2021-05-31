703410bdf66b7e2514d0ded2847d4542dbdf408a
prateek <prateek.asthana@atscale.com>
2021-05-31 00:06:53 -0700

    working version

--- src/main/scala/com/prateek/uniquephoto/Main.scala
+++ src/main/scala/com/prateek/uniquephoto/Main.scala
@@ -4,7 +4,8 @@ import java.nio.file.Paths
 
 object Main extends App {
   Traversor
-//    .traverse(Paths.get("/Users/prateek/code/pattu/unique-photo-directories"))
+//    .traverse(Paths.get("/"))
+    //    .traverse(Paths.get("/Users"))
     .traverse(Paths.get("/Volumes/FreeAgent Drive/backup"))
     .foreach(println)
 }
--- src/main/scala/com/prateek/uniquephoto/Traversor.scala
+++ src/main/scala/com/prateek/uniquephoto/Traversor.scala
@@ -1,54 +1,92 @@
 package com.prateek.uniquephoto
 
 import scala.jdk.StreamConverters._
+import scala.util.Using
 
-import java.nio.file.{ Files, Path }
+import java.io.File
+import java.nio.file.{ Files, Path, Paths }
 
 object Traversor {
 
   type ImageDirectoryTuple = (Option[ImageDirectory], Seq[ImageDirectory])
 
   def traverse(root: Path): Seq[ImageDirectory] = {
-    val scalaPaths: LazyList[Path] = Files.list(root).toScala(LazyList)
+
+    val scalaPaths: List[String] =
+      Using(Files.list(root)) { s => s.toScala(List).map(_.toString) }.get
+
     val childPaths: ImageDirectoryTuple =
       scalaPaths.foldLeft(ImageDirectoryTuple)((idt, path) => {
         path match {
+          case ExcludeDirectory() => idt
           case Directory() =>
-            val childImageDirectories = traverse(path)
+            val childImageDirectories = traverse(Paths.get(path))
             val updatedImageDirectories =
               idt._2.appendedAll(childImageDirectories)
             (idt._1, updatedImageDirectories)
           case ImageFile() =>
             val newIdt = idt._1
               .map(id => {
-                val updatedFiles = id.files.prepended(path.getFileName.toString)
+                val updatedFiles = id.files.prepended(path)
                 id.copy(files = updatedFiles)
               })
               .orElse(
-                Some(ImageDirectory(root, Seq(path.getFileName.toString)))
+                Some(ImageDirectory(root, Seq(path)))
               )
             (newIdt, idt._2)
-          case _ => idt
+          case _ =>
+//            println(s"excluding: $path")
+            idt
         }
       })
-    childPaths._1
+
+    val ret = childPaths._1
       .map(id => childPaths._2.prepended(id))
       .getOrElse(childPaths._2)
+//    println(s"ImageDirectories parsed for root: $root $ret")
+    ret
   }
 
   // refer to https://alvinalexander.com/scala/scala-type-examples-type-aliases-members/
   def ImageDirectoryTuple: ImageDirectoryTuple = (None, Seq.empty)
 
+  object ExcludeDirectory {
+    val excludeDirectories: Set[Path] = Set(
+      "/usr/sbin/authserver",
+      "/usr/lib/cron",
+      "/etc/cups/certs",
+      "/var",
+      "/Library",
+      "/Users/prateek/Library",
+      "/Users/Shared",
+      "/Users/prateek/.Trash"
+    ).map(Paths.get(_))
+
+    val endsWith: Set[String] =
+      Set(
+        "docs",
+        "entertainment",
+        "sangeet",
+        "eclipse-jee-galileo-SR2-win32",
+        "books",
+        "ProgramFiles"
+      )
+
+    def unapply(strPath: String): Boolean = {
+      val path = Paths.get(strPath)
+
+      excludeDirectories.contains(path) ||
+      endsWith.exists(e => path.endsWith(e))
+    }
+  }
+
   object Directory {
-    def unapply(path: Path): Boolean = path.toFile.isDirectory
+    def unapply(path: String): Boolean = new File(path).isDirectory
   }
 
   object ImageFile {
-    import org.apache.tika.Tika
-    val tika = new Tika
-
-    def unapply(path: Path): Boolean = {
-      val bool = path.toString.matches("(?i).+\\.(jpg|avi)$")
+    def unapply(path: String): Boolean = {
+      val bool = path.matches("(?i).+\\.(jpg|avi)$")
 //      println(s"file: ${path.getFileName}, bool: $bool")
       bool
     }
@@ -57,6 +95,6 @@ object Traversor {
 
 case class ImageDirectory(dir: Path, files: Seq[String]) {
   override def toString: String = {
-    s"dir: $dir, \r\nfile: ${files.foreach(println)}\r\n"
+    s"dir: $dir, \r\nfile: $files\r\n"
   }
 }
